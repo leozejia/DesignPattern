8b8ce44b0f147442d4d5b663e0cd1c640eea1b95
leozejia <44258909+leozejia@users.noreply.github.com>
2020-11-22 15:23:05 +0800

    Factory

---
+++ DesignPattern/Assets/Scripts/Factory/Creator_GenericcClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creator_GenericcClass<T> where T:Product, new()
+{
+    public Creator_GenericcClass()
+    {
+        Debug.Log("产生工程：Creator_GenericClass<" + typeof(T).ToString() + ">");
+    }
+
+    public Product FactoryMethod()
+    {
+        return new T();
+    }
+}
+
+public abstract class Product
+{ 
+    
+}
+
+interface Creator_GenericMethod
+{
+    Product FactoryMethod<T>() where T : Product, new();
+}
+
+public class ConcreteCreator_GenericMethod : Creator_GenericMethod
+{
+    public ConcreteCreator_GenericMethod()
+    {
+        Debug.Log("产生工厂：ConcreteCreator_GenericMethod");
+    }
+
+    public Product FactoryMethod<T>() where T : Product, new()
+    {
+        return new T();
+    }
+}
